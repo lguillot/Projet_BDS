b1d47abdb2fa040bcbb539fcdc9096928c920816
Louis Guillot <lg.guillot@gmail.com>
2012-02-20 13:40:30 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjetBds::Application.config.secret_token = '0790a7d3786e6f47b481e9615c0cbfd011736461392b35b432d4cc5b7c03f2ab6408acca4d82c59bfc25683b94f1de744ff7e1a506a8cd6fe0de54097b5fdbfa'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ProjetBds::Application.config.session_store :cookie_store, :key => '_Projet_BDS_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ProjetBds::Application.config.session_store :active_record_store
